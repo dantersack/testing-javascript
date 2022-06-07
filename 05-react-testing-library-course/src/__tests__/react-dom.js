b0b1bb3770f3650c8ada597268d1f83a86cc726b
Dante Sack <sack_dante@hotmail.com>
2022-06-07 17:17:12 -0300

    05/use react-testing-library to render and test react components

--- 05-react-testing-library-course/src/__tests__/react-dom.js
+++ 05-react-testing-library-course/src/__tests__/react-dom.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import {getQueriesForElement} from '@testing-library/dom';
+import {render} from '@testing-library/react';
 import {FavoriteNumber} from '../favorite-number';
 
 test('renders a number input with a label "Favorite Number"', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<FavoriteNumber />, div);
-  console.log(div.innerHTML);
-  const {getByLabelText} = getQueriesForElement(div);
+  const {container, getByLabelText} = render(<FavoriteNumber />);
+  console.log(container.innerHTML);
   const input = getByLabelText(/favorite number/i);
   expect(input).toHaveAttribute('type', 'number');
 });
