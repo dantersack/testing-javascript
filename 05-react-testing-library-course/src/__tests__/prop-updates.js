47e76e251befa4a6e266861b649e148894e8fb11
Dante Sack <sack_dante@hotmail.com>
2022-06-11 21:18:02 -0300

    05/assert that something is not rendered with react testing library

--- 05-react-testing-library-course/src/__tests__/prop-updates.js
+++ 05-react-testing-library-course/src/__tests__/prop-updates.js
@@ -4,14 +4,13 @@ import {render} from '@testing-library/react';
 import {FavoriteNumber} from '../favorite-number';
 
 test('entering an invalid number shows an error message', () => {
-  const {getByLabelText, getByRole, rerender, debug} = render(
+  const {getByLabelText, getByRole, queryByRole, rerender} = render(
     <FavoriteNumber />,
   );
   const input = getByLabelText('Favorite Number');
   user.type(input, '10');
   const errMsg = getByRole('alert');
   expect(errMsg).toHaveTextContent('The number is invalid');
-  debug();
   rerender(<FavoriteNumber max={10} />);
-  debug();
+  expect(queryByRole('alert')).toBeNull();
 });
